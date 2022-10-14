d0336301ec547a7107a16560a58e3dbc205987dd
diaoling <1178530422@qq.com>
2022-10-14 10:36:24 +0800

    修改后版本

---
+++ go_homework/go_10_10_homework/search_test.go
@@ -0,0 +1,49 @@
+package go_10_10_homework
+
+import (
+	"testing"
+)
+
+// go test -run ^Testsearch$ ./go_10_10_homework
+func TestSearch(t *testing.T) {
+	data := []struct {
+		nums   []int
+		target int
+		answer int
+	}{
+		{[]int{-1, 0, 3, 5, 9, 12}, 4, -1},
+		{[]int{-1, 0, 3, 5, 9, 12}, 3, 2},
+		{[]int{-1, 0, 3, 5, 9, 12}, 12, 5},
+	}
+	for _, v := range data {
+		x := Search(v.nums, v.target) //获取返回值
+		if x != v.answer {
+			t.Fatalf("expect:[%v] != result[%v]", x, v.answer)
+		}
+
+	}
+}
+func Search(nums []int, target int) int {
+	var i int = len(nums) - 1
+	var j int = 0
+	var mid int = len(nums) / 2
+	for i >= j {
+		mid = (i + j) / 2
+		if nums[mid] == target {
+			return mid //符合条件，查找成功跳出循环，减少时间消耗
+
+		}
+		if nums[mid] < target {
+			j = mid + 1
+		}
+		if nums[mid] > target {
+			i = mid - 1
+		}
+
+	}
+	//fmt.Println(mid)
+	if nums[mid] != target { //查找到最后找不到就把结果赋值为-1返回出去
+		mid = -1
+	}
+	return mid
+}
---
+++ go_homework/go_10_10_homework/searchinsert_test.go
@@ -0,0 +1,40 @@
+package go_10_10_homework
+
+import (
+	"testing"
+)
+
+func TestSearchInsert(t *testing.T) {
+
+	data := []struct {
+		nums   []int
+		target int
+		answer int
+	}{
+		{[]int{1, 3, 5, 6}, 5, 2},
+		{[]int{1, 3, 5, 6}, 20, 4},
+		{[]int{1, 3, 5, 6}, 1, 0},
+	}
+	for _, v := range data {
+		result := SearchInsert(v.nums, v.target)
+		if result != v.answer {
+			t.Fatalf("expect:[%v] != result[%v]", result, v.answer)
+		}
+	}
+
+}
+
+func SearchInsert(nums []int, target int) int { //和内部函数重复，改名后提交通过
+	var i int = 1
+	for key, value := range nums {
+		//fmt.Println(key, value, len(nums))//测试代码
+
+		if value >= target { //判断输入key和value值关系
+			return key
+		}
+		if value < target && key == len(nums)-1 { //超出范围会返回最后的位置
+			i = len(nums)
+		}
+	}
+	return i
+}
